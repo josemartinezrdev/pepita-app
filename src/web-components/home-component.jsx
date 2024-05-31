d008367617a8d375398a24f1488554ed6a62c0f5
josemartinezrdev <josemartinezrdev@gmail.com>
2024-05-30 19:43:09 -0500

    build: :construction: Btn Back

--- src/web-components/home-component.jsx
+++ src/web-components/home-component.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { MateriaComponent } from "./materia-component";
 
 const Home = () => {
+  const [first, setFirst] = useState(true);
+  const [second, setSecond] = useState(false);
+
+  const btnClick = () => {
+    setFirst(false);
+    setSecond(true);
+  };
+
   return (
     <>
-      <h1>Eli empiezas aquí 🙂</h1>
+      {first ? (
+        <div className="container-home">
+          <img src="public/imgs/flores.png" alt="" className="lila" />
+          <h1>Bienvenido a PeppitaApp</h1>
+          <h2>¿Qué Deseas Hacer Hoy?</h2>
+          <div className="option-home">
+            <div
+              className="options-mPrima options"
+              tabIndex="0"
+              onClick={btnClick}
+            >
+              <img src="public/imgs/materiaPrima.png" alt="" />
+              <p>Gestionar Materia Prima</p>
+            </div>
+            <div className="options-mObra options" tabIndex="0">
+              <img src="public/imgs/manoObra.png" alt="" />
+              <p>Mano de Obra</p>
+            </div>
+            <div className="options-cIndirectos options" tabIndex="0">
+              <img src="public/imgs/costIndirectos.png" alt="" />
+              <p>Costos Indirectos</p>
+            </div>
+            <div className="options-cInformes options" tabIndex="0">
+              <img src="public/imgs/informes.png" alt="" />
+              <p>Informes</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      {second ? <MateriaComponent /> : null}
     </>
   );
 };
---
+++ src/web-components/materia-component.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { FormMateria } from "./form-materia-prima";
+import Home from "./home-component";
+
+export const MateriaComponent = () => {
+  const [first, setFirst] = useState(true);
+  const [second, setSecond] = useState(false);
+  const [back, setBack] = useState(false);
+
+  const btnClick = (id) => {
+    setFirst(false);
+    setSecond(true);
+    window.globalOption = id;
+  };
+
+  const btnBack = () => {
+    setBack(true);
+  };
+
+  if (back) {
+    return <Home />;
+  }
+
+  return (
+    <>
+      {first ? (
+        <div className="container-home">
+          <button className="button" onClick={btnBack}>
+            ← Volver
+          </button>
+          <img src="public/imgs/flores.png" alt="" className="lila" />
+          <h1>Seleccione Tipo de Materia Prima</h1>
+          <div className="option-home">
+            <div
+              className="options-mPrima options"
+              tabIndex="0"
+              onClick={() => btnClick("tela")}
+            >
+              <img src="public/imgs/tela.png" alt="" />
+              <p>Telas</p>
+            </div>
+            <div
+              className="options-mObra options"
+              tabIndex="0"
+              onClick={() => btnClick("hilo")}
+            >
+              <img src="public/imgs/hilo.png" alt="" />
+              <p>Hilos</p>
+            </div>
+            <div
+              className="options-cIndirectos options"
+              tabIndex="0"
+              onClick={() => btnClick("boton")}
+            >
+              <img src="public/imgs/botones.png" alt="" />
+              <p>Botones</p>
+            </div>
+            <div
+              className="options-cInformes options"
+              tabIndex="0"
+              onClick={() => btnClick("cierre")}
+            >
+              <img src="public/imgs/cremallera.png" alt="" />
+              <p>Cierres</p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      {second ? <FormMateria /> : null}
+    </>
+  );
+};
