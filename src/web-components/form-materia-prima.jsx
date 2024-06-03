a4fc2478d33cd036269a3c81ae6f11b277526473
JhonJMD <152086681+JhonJMD@users.noreply.github.com>
2024-06-03 12:20:50 -0500

    chore: :speech_balloon: Add comments
    Code operation comments are added for these components.

--- src/web-components/form-materia-prima.jsx
+++ src/web-components/form-materia-prima.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { MateriaComponent } from "./materia-component";
-import { getData, putData } from "../api-services/api-service.jsx";
+import React, { useState } from "react"; // Importacion de React y useState funcion nativa de React
+import { MateriaComponent } from "./materia-component"; // Importacion del componente MateriaComponent
+import { getData, putData } from "../api-services/api-service.jsx"; // Importacion de las funciones para gestiones la API
 
 export const FormMateria = () => {
-  const [back, setBack] = useState(false);
-  const [formData, setFormData] = useState({
+  const [back, setBack] = useState(false); // Estado para gestionar el boton de retroceso
+  const [formData, setFormData] = useState({ // Estado para los datos de la materia prima
     nameProveedor: '',
     ubicacion: '',
     Cantidad: '',
@@ -13,29 +13,34 @@ export const FormMateria = () => {
     notas: ''
   });
 
+  // Funcion para gestionar el boton de retroceso
   const btnBack = () => {
-    setBack(true);
+    setBack(true); // Actualiza el estado para retroceder
   };
 
+  // Renderiza el componente si el estado de back es true
   if (back) {
     return <MateriaComponent />;
   }
 
+  // Funcion para manejar los cambios de los inputs del formulario
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    const { name, value } = e.target; // Destructuracion de name y value del evento
+    setFormData((prevData) => ({ ...prevData, [name]: value })); // Actualiza del estado del formulario
   };
 
+  // Funcion para manejar el envio del formulario
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Previene el comportamiento predeterminado del formulario
     try {
-      const id = window.id;
-      const baseUrl = "https://665637279f970b3b36c4a8f5.mockapi.io";
-      const endpoint = "MateriaPrima";
+      const id = window.id; // Se guarda la ID del elemento seleccionado de la variable global
+      const baseUrl = "https://665637279f970b3b36c4a8f5.mockapi.io"; // URL base de la API
+      const endpoint = "MateriaPrima"; // Endpoint para acceder a materia prima
 
       // Obtener los datos actuales del elemento a actualizar
       const currentData = await getData(baseUrl, endpoint, id);
       const newStock = Number(currentData.cantidadStock) + Number(formData.Cantidad);
+
       // Actualizar solo los campos del formulario en los datos actuales
       const updatedData = {
         ...currentData,
@@ -45,17 +50,19 @@ export const FormMateria = () => {
         descripcion: formData.Descripcion,
         costoUnidad: Number(formData.costo),
         notasAdicionales: formData.notas,
-        fechaAdquisicion: new Date().toISOString().split("T")[0],
+        fechaAdquisicion: new Date().toISOString().split("T")[0] // Actualiza la fecha de adquisicion con la fecha actual del momento que se envian los datos
       };
 
-      await putData(baseUrl, endpoint, updatedData, id);
-      alert("Datos actualizados correctamente");
-      setBack(true);
+      await putData(baseUrl, endpoint, updatedData, id); // Enviar los datos actualizados a la API
+      alert("Datos actualizados correctamente"); // Muestra un mensaje de exito
+      setBack(true); // Vuelve a home despues de enviar los datos
     } catch (error){
-      console.error('Error al actualizar los datos:', error);
+      console.error('Error al actualizar los datos:', error); // Manejo de errores
     }
   };
 
+
+  // Renderiza el html con sus respectivos eventos y variables configuradas anteriormente
   return (
     <>
       <div className="container-formMP">
--- src/web-components/informe-component.jsx
+++ src/web-components/informe-component.jsx
@@ -1,39 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { getData, getDataAll } from "../api-services/api-service";
-import {Home} from "./home-component";
+import React, { useState, useEffect } from "react"; // Importacion de React y las funciones nativas de React
+import { getData, getDataAll } from "../api-services/api-service"; // Importacionde las funciones para manejo de datos de la API
+import {Home} from "./home-component"; // Importacionde de Home
 
 export const InformeComponent = () => {
-    const [data, setData] = useState(null);
-    const [lotes, setLotes] = useState([]);
-    const [LoteId, setLoteId] = useState('');
-    const [back, setBack] = useState(false);
+    const [data, setData] = useState(null); // Estado para almacenar la data del lote seleccionado
+    const [lotes, setLotes] = useState([]); // Estado para almacenar la lista a seleccionar de lotes
+    const [LoteId, setLoteId] = useState(''); // Estado para almacenar eñ ID del lote seleccionado
+    const [back, setBack] = useState(false); // Estado para gestionar el boton hacia atras
 
+    // useEffect para obtener la lista de lotes cuadno el componente se monta
     useEffect(() => {
         const fetchLotes = async () => {
             try {
-                const baseUrl = 'https://665637279f970b3b36c4a8f5.mockapi.io';
-                const endpoint = 'Lotes';
-                const lotesData = await getDataAll(baseUrl, endpoint);
-                setLotes(lotesData);
+                const baseUrl = 'https://665637279f970b3b36c4a8f5.mockapi.io'; // Url base de la API
+                const endpoint = 'Lotes'; // Endpoint para obtener los lotes
+                const lotesData = await getDataAll(baseUrl, endpoint); // Obtener los datos de todos los lotes almacenados en la API
+                setLotes(lotesData); // Alturaliza el estado con la lista de lotes
             } catch (error) {
-                console.error('Error al obtener los lotes:', error);
+                console.error('Error al obtener los lotes:', error); // Manejo de errores
             }
         };
 
         fetchLotes();
     }, []);
 
+    // useEffect para obtener los detalles del lote seleccionado
     useEffect(() => {
         if (LoteId) {
             const fetchLotDetails = async () => {
                 try {
-                    const baseUrl = 'https://665637279f970b3b36c4a8f5.mockapi.io';
-                    const endpoint = 'Lotes';
-                    const id = LoteId;
-                    const loteData = await getData(baseUrl, endpoint, id);
-                    setData(loteData);
+                    const baseUrl = 'https://665637279f970b3b36c4a8f5.mockapi.io'; // URL base de la API
+                    const endpoint = 'Lotes'; // Endpoint para obtener los lotes
+                    const id = LoteId; // ID del lote seleccionado
+                    const loteData = await getData(baseUrl, endpoint, id); // Obtener los datos del lote seleccionado
+                    setData(loteData); // Actualiza el estado con la data del lote seleccionado
                 } catch (error) {
-                    console.error('Error al obtener los detalles del lote:', error);
+                    console.error('Error al obtener los detalles del lote:', error); // Manejo de errores
                 }
             };
 
@@ -41,18 +43,22 @@ export const InformeComponent = () => {
         }
     }, [LoteId]);
 
+    // Funcion para manejar el cambio de la seleccion del lote
     const handleLotChange = (e) => {
-        setLoteId(e.target.value);
+        setLoteId(e.target.value); // Actuliza el ID del lote seleccionado
     };
 
+    // Funcion para manejar el boton hacia atras
     const btnBack = () => {
-        setBack(true);
+        setBack(true); // Actualiza el estado para volver hacia atras
     };
 
+    // Renderiza el componente si el estado de back es true
     if (back) {
         return <Home />;
     }
 
+    // Renderiza el html con sus respectivos eventos y variables configuradas anteriormente
     return (
         <>
             <div className="container-infoLote">
